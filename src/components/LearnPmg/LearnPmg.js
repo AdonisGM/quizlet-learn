a43849c40e684b4b0e2dc1c5ad1fbe48c1237db6
Nguyen Manh Tung <nmtung.study@gmail.com>
2022-08-15 06:29:28 +0700

    Fix bug progress in learn screen

--- src/components/LearnPmg/LearnPmg.js
+++ src/components/LearnPmg/LearnPmg.js
@@ -129,7 +129,7 @@ const Learn = () => {
 
       setNumberLearning((countLearn * 100) / totalL);
     }
-  }, [selectAnswer, indexSelectQuestion, listLearning]);
+  }, [selectAnswer, indexSelectQuestion, listLearning, showResult]);
 
   // function and handler
   const generateAnswer = (answer) => {
